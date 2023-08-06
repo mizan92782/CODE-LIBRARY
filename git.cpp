0a269f2bf68285edfe24088f10c4120e50ef20bb
mizanur-rahman <mizanmd92782@gmail.com>
2023-08-06 16:33:08 +0600

    adding git file

---
+++ git.cpp
@@ -0,0 +1,30 @@
+/*
+** working directory stage
+git intitiatization  -> git init
+
+
+
+** staging stage
+git status  -> git status
+git adding  -> git add -A
+git adding  -> git add -filename
+git restorinhg -> git resotore filename
+git config user.name/user.email
+
+
+*** local reposotiry stage
+  git commit -m "message"
+ history  -git log
+
+
+*** rempter repoeototy
+ git remote to check
+ git remote link
+ git clone link
+
+
+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
+
+
+*/
+
